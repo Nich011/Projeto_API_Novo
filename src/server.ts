9bb18311806993b955e86142d5015f402cd7e9e9
Nich011 <nicgavi01@gmail.com>
2024-06-18 17:37:12 -0300

    ok

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import express, {Request, Response} from "express";
 const app = express()
 
 app.get('/teste', (req : Request, res : Response) => {
-    res.send('Teste')
+    res.send('Teste de funcionamento da API')
 })
 
 var mysql = require('mysql');
@@ -13,21 +13,30 @@ var connection = mysql.createConnection({
     user: "root",
     password: "Nicholas01**",
     database: "projeto_api"
-  });
+});
 
-app.post('/enviar', () => {
+app.use(express.urlencoded({ extended: true }));
+
+app.post('/enviar', (req: Request, res: Response) => {
     connection.connect(function(err:Error) {
         if (err) throw err;
         console.log("Conectado ao Banco de Dados!");
-        var sql = "INSERT INTO corretores (emp_num_cnpj, name, corporate_reason, email, number, consultancy) VALUES (12345678901234, 'Teste', 'Teste Razao Social', 'teste@email.com', 111234567890, 'Teste Acessoria')";
-        connection.query(sql, function (err:Error, result:any) {
+
+        let emp_num_cnpj : unknown = req.body.emp_num_cnpj
+        let name : unknown = req.body.name
+        let corporate_reason : unknown = req.body.corporate_reason
+        let email : unknown = req.body.email
+        let number : unknown = req.body.number
+        let consultancy : unknown = req.body.consultancy
+
+        var sql = `INSERT INTO corretores (emp_num_cnpj, name, corporate_reason, email, number, consultancy) VALUES (${emp_num_cnpj}, ${name}, ${corporate_reason}, ${email}, ${number}, ${consultancy})`;
+        connection.query(sql, function (err:Error) {
           if (err) throw err;
-          console.log("Cadastro adicionado.");
+          console.log("Cadastro adicionado:");
         });
-      });
+
+        res.send(`O Cadastro com CNPJ ${emp_num_cnpj} foi adicionado com sucesso.`);
+    });
 })
 
-
 app.listen(3000, () => console.log('Servidor iniciado na porta 3000'))
-
-export default app;
